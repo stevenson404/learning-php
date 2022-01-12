28ea910fa9495dcf462183ee052d969159781d6c
stevenson404 <stevenson404@users.noreply.github.com>
2022-01-12 22:47:08 +0000

    First versions of php files

---
+++ case-vars.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+	<?php
+		$color = "red";
+		ECHO "Hello World." . $color . "</br>";
+		eChO "Hello World." . $ColoR . "</br>";
+		echo "Hello World." . $coLor . "</br>";
+	?>
+</body>
+</html>
